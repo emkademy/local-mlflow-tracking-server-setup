0036df42e942b8c2d3b1e507dd2a47165629c240
Kıvanç Yüksel <emkademy@gmail.com>
2024-05-24 13:59:13 +0200

    checkpoint

--- docker/run-server.sh
+++ docker/run-server.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-
-# In this script we are going to run the mlflow tracking server with the backend store and artifact store
-# we defined earlier. So, this will be a very simple script.
-
 mlflow server \
-    --backend-store-uri "${MLFLOW_BACKEND_STORE}" \
-    --default-artifact-root "${MLFLOW_ARTIFACT_STORE}" \
-    --host 0.0.0.0 \
-    --port "${LOCAL_DEV_MLFLOW_SERVER_PORT}"
-
-# Now, finally, let's create a Make file, then build our Docker image
+	--backend-store-uri "${MLFLOW_BACKEND_STORE}" \
+	--default-artifact-root "${MLFLOW_ARTIFACT_STORE}" \
+	--host 0.0.0.0 \
+	--port "${LOCAL_DEV_MLFLOW_SERVER_PORT}"
